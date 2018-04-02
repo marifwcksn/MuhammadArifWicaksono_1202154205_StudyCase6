d4f15b1dfb907b3c8e1bd73daf53341c20ff310f
marifwcksn <marifwcksn@gmail.com>
2018-04-03 00:44:52 +0700

    push pertama kali

---
+++ app/src/main/java/com/example/arif/muhammadarifwicaksono_1202154205_studycase6/model/User.java
@@ -0,0 +1,29 @@
+package com.example.arif.muhammadarifwicaksono_1202154205_studycase6.model;
+
+public class User {
+
+    String userId;
+    String username;
+    String email;
+
+    public User() {
+    }
+
+    public User(String userId, String username, String email) {
+        this.userId = userId;
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
